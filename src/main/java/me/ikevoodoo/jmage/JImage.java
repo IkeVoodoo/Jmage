85d8681e0b3cc9506e5f44a75f589dc6c7a79423
IkeVoodoo2 <simone.coletti07@gmail.com>
2022-05-11 10:02:04 +0200

    Added some methods and refined some others

--- src/main/java/me/ikevoodoo/jmage/JImage.java
+++ src/main/java/me/ikevoodoo/jmage/JImage.java
@@ -3,8 +3,10 @@ package me.ikevoodoo.jmage;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
+import java.util.function.Function;
 
 public class JImage {
 
@@ -15,26 +17,70 @@ public class JImage {
     }
 
     public JImage resize(int width, int height) {
-        Image scaled = this.image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        this.image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics g = this.image.getGraphics();
-        g.drawImage(scaled, 0, 0, null);
+        BufferedImage img = new BufferedImage(width, height, this.image.getTransparency());
+        Graphics2D g = (Graphics2D) img.getGraphics();
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED);
+        g.drawImage(this.image, 0, 0, width, height, null);
         g.dispose();
-        return this;
-    }
-
-    public JImage mask(JImage mask) {
-        BufferedImage img = new BufferedImage(this.image.getWidth(), this.image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        for (int x = 0; x < this.image.getWidth(); x++)
-            for (int y = 0; y < this.image.getHeight(); y++)
-                img.setRGB(x, y, mask.image.getRGB(x, y) == 0xFFFFFFFF ? this.image.getRGB(x, y) : 0x00000000);
         this.image = img;
         return this;
     }
 
+    public JImage mask(JImage mask) {
+        int width = Math.min(this.image.getWidth(), mask.image.getWidth());
+        int height = Math.min(this.image.getHeight(), mask.image.getHeight());
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        WritableRaster maskRaster = mask.image.getRaster();
+        WritableRaster imageRaster = image.getRaster();
+        execute(img, width, height, data -> {
+            maskRaster.getPixel(data.x, data.y, data.pixels);
+            if(data.pixels[0] == 255 && data.pixels[1] == 255 && data.pixels[2] == 255)
+                imageRaster.getPixel(data.x, data.y, data.pixels);
+            else {
+                if(data.pixels.length == 4)
+                    data.pixels[3] = 0;
+                else {
+                    data.pixels[0] = 0;
+                    data.pixels[1] = 0;
+                    data.pixels[2] = 0;
+                }
+            }
+            return data.pixels;
+        });
+        this.image = img;
+        return this;
+    }
+
+    public JImage replace(Color color, Color replacement) {
+        return replace(color, replacement, 15);
+    }
+
+    public JImage replace(Color color, Color replacement, int tolerance, Color... exclude) {
+        int rgb = replacement.getRGB();
+        int colorRgb = color.getRGB();
+        WritableRaster raster = this.image.getRaster();
+        execute(this.image, this.image.getWidth(), this.image.getHeight(), data -> {
+            raster.getPixel(data.x, data.y, data.pixels);
+            int pixelRgb = data.pixels[0] << 16 | data.pixels[1] << 8 | data.pixels[2];
+            if(distance(pixelRgb, colorRgb) <= tolerance) {
+                for (Color excludeColor : exclude) {
+                    if(distance(pixelRgb, excludeColor.getRGB()) <= tolerance)
+                        return data.pixels;
+                }
+                data.pixels[0] = (rgb >> 16) & 0xFF;
+                data.pixels[1] = (rgb >> 8) & 0xFF;
+                data.pixels[2] = rgb & 0xFF;
+                if(data.pixels.length == 4)
+                    data.pixels[3] = (rgb >> 24) & 0xFF;
+            }
+            return data.pixels;
+        });
+        return this;
+    }
+
     public JImage write(String path) throws IOException {
-        String extension = path.contains(".") ? path.substring(path.lastIndexOf(".") + 1) : "png";
-        ImageIO.write(this.image, extension, new File(path));
+        ImageIO.write(this.image, path.contains(".") ? path.substring(path.lastIndexOf(".") + 1) : "png", new File(path));
         return this;
     }
 
@@ -55,8 +101,34 @@ public class JImage {
         return this;
     }
 
+    private void execute(BufferedImage image, int width, int height, Function<ExecuteData, int[]> function) {
+        WritableRaster raster = image.getRaster();
+        ExecuteData data = new ExecuteData();
+        data.pixels = new int[image.getColorModel().getNumComponents()];
+        for (data.x = 0; data.x < width; data.x++) {
+            for (data.y = 0; data.y < height; data.y++) {
+                raster.setPixel(data.x, data.y, function.apply(data));
+            }
+        }
+    }
+
+    private int distance(int rgb1, int rgb2) {
+        int r1 = (rgb1 >> 16) & 0xFF;
+        int g1 = (rgb1 >> 8) & 0xFF;
+        int b1 = rgb1 & 0xFF;
+        int r2 = (rgb2 >> 16) & 0xFF;
+        int g2 = (rgb2 >> 8) & 0xFF;
+        int b2 = rgb2 & 0xFF;
+        return (r1 - r2) * (r1 - r2) + (g1 - g2) * (g1 - g2) + (b1 - b2) * (b1 - b2);
+    }
+
     @Override
     public String toString() {
         return "JImage[image=" + this.image + "]";
+    }
+
+    private class ExecuteData {
+        public int x, y;
+        public int[] pixels;
     }
 }
--- src/main/java/me/ikevoodoo/jmage/JMage.java
+++ src/main/java/me/ikevoodoo/jmage/JMage.java
@@ -2,31 +2,38 @@ package me.ikevoodoo.jmage;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.Optional;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 public class JMage {
 
     private JMage() {}
 
     public static Future<Optional<JImage>> read(String url) throws IOException {
-        URL u = new URL(url);
-        HttpURLConnection conn = (HttpURLConnection) u.openConnection();
-        conn.setInstanceFollowRedirects(true);
-        conn.setConnectTimeout(5000);
-        conn.setReadTimeout(5000);
-        conn.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36");
-        conn.setRequestProperty("accept", "*/*");
-        conn.setRequestProperty("accept-encoding", "gzip, deflate, br");
-        conn.setRequestProperty("keep-alive", "true");
-        conn.setRequestProperty("host", u.getHost());
-        conn.connect();
-        return JMage.read(conn.getInputStream());
+        return JMage.read(getConnection(url).getInputStream());
+    }
+
+    public static Stream<Future<Optional<JImage>>> read(String... urls) {
+        return Stream.of(urls).map(url -> {
+            try {
+                return JMage.read(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        });
+    }
+
+    public static void pipe(String url, File output) throws IOException {
+        Files.copy(getConnection(url).getInputStream(), output.toPath());
     }
 
     public static Future<Optional<JImage>> read(InputStream is) {
@@ -67,5 +74,20 @@ public class JMage {
                 return get();
             }
         };
+    }
+
+    private static HttpURLConnection getConnection(String url) throws IOException {
+        URL u = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+        conn.setInstanceFollowRedirects(true);
+        conn.setConnectTimeout(5000);
+        conn.setReadTimeout(5000);
+        conn.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36");
+        conn.setRequestProperty("accept", "*/*");
+        conn.setRequestProperty("accept-encoding", "gzip, deflate, br");
+        conn.setRequestProperty("keep-alive", "true");
+        conn.setRequestProperty("host", u.getHost());
+        conn.connect();
+        return conn;
     }
 }
